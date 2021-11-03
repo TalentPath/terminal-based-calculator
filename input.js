5b54a3ca3ff44925f1296599235751a677494f6f
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-03 11:28:08 -0500

    Updated code to accept floats and other numbers

--- input.js
+++ input.js
@@ -8,8 +8,8 @@ let commandList = ["add", "subtract", "divide", "multiply", "remainder", "expone
 if (args.length != 5)
 {
     console.log();
-    console.log("You entered an invalid number of arguments");
-    console.log("Please enter a command followed by only two integer numbers");
+    console.log(`You entered ${args.length-2} arguments, which is invalid`);
+    console.log("Please enter a command followed by only two numbers as arguements");
     console.log("Valid commands are:");
     console.log(commandList);
     console.log();
@@ -19,52 +19,55 @@ if (args.length != 5)
 let int1 = 0;
 let int2 = 0;
 
+let num1 = 0;
+let num2 = 0;
+
 try
 {
-    let num1 = Number(args[3]);
+    num1 = Number(args[3]);
 
-    try
-    {
-        int1 = parseInt(num1);
-    } catch (error)
-    {
-        console.log();
-        console.log("You entered an invalid type of number for the first number.");
-        console.log("Please try again with only integer numbers");
-        console.log();
-        process.exit();
-    }
+    // try
+    // {
+    //     int1 = parseInt(num1);
+    // } catch (error)
+    // {
+    //     console.log();
+    //     console.log("You entered an invalid type of number for the first number.");
+    //     console.log("Please try again with only integer numbers");
+    //     console.log();
+    //     process.exit();
+    // }
 }
 catch (error)
 {
     console.log();
     console.log("You entered an invalid argument for the first number.");
-    console.log("Please try again with only integer numbers");
+    console.log("Please try again with only numbers");
     console.log();
     process.exit();
 }
 
 try
 {
-    let num2 = Number(args[4]);
+    num2 = Number(args[4]);
 
-    try
-    {
-        int2 = parseInt(num2);
-    } catch (error)
-    {
-        console.log();
-        console.log("You entered an invalid type of number for the second number.");
-        console.log("Please try again with only integer numbers");
-        console.log();
-        process.exit();
-    }
+    // try
+    // {
+    //     int2 = parseInt(num2);
+    // } catch (error)
+    // {
+    //     console.log();
+    //     console.log("You entered an invalid type of number for the second number.");
+    //     console.log("Please try again with only integer numbers");
+    //     console.log();
+    //     process.exit();
+    // }
 }
 catch (error)
 {
     console.log();
     console.log("You entered an invalid argument for the second number.");
-    console.log("Please try again with only integer numbers");
+    console.log("Please try again with only numbers");
     console.log();
     process.exit();
 }
@@ -88,9 +91,13 @@ if (int2 > Number.MAX_SAFE_INTEGER)
 try
 {
     command = args[2];
-    mycalc.calc[command](int1, int2);
+    mycalc.calc[command](num1, num2);
 } catch (error)
 {
-    console.log("Something went wrong!");
-    console.log("Error:", error);
+    console.log();
+    console.log("You entered an invalid command,", command);
+    console.log("Valid commands are:");
+    console.log(commandList);
+    console.log();
+    process.exit();
 }
