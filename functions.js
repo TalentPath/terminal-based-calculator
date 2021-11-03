a3cdd7dfc81a2140f5caeb4924d1394af1595790
Chase Faggard <chasefaggard@Chases-MacBook-Pro.local>
2021-11-03 11:15:29 -0500

    Working

---
+++ functions.js
@@ -0,0 +1,12 @@
+// Calculator functions
+
+const functions = {
+    add(a, b) { return a + b; },
+    subtract(a, b) { return a - b; },
+    multiply(a, b) { return a * b; },
+    divide(a, b) { return a / b; },
+    remainder(a, b) { return a % b; },
+    exponent(a, b) { return a ** b; }
+}
+
+module.exports = {functions }; // Export module
---
+++ main.js
@@ -0,0 +1,16 @@
+const { functions } = require('./functions.js') // Import function object
+
+const args =  process.argv; // Get arguments
+
+if(args.length == 5) { // Check length
+    const num1 = parseInt(args[3]);
+    const num2 = parseInt(args[4]);
+    if(!isNaN(num1) && !isNaN(num2)) { // If last two arguments are numbers
+        try {
+            console.log(functions[args[2].toLowerCase()](num1, num2));
+        } catch (error) {
+            console.log("Command not found.");
+        }
+    } else { console.log("Please enter a number for the last 2 arguments"); }
+} else { console.log("Incorrect number of arguments"); }
+
